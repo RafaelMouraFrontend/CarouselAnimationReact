dfcafc8300d7ed3056cb1b538176f34635091020
RafaelMouraFrontend <rafael.mouraweb@gmail.com>
2019-08-14 02:57:16 +0000

    ajustes no componente

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(
-    <App />, 
+    <App />,
     document.getElementById('root')
 );
-registerServiceWorker();
